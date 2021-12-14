e87b5f4e5386878f458c0d67fcad2925519a54bb
sgloayza <sgloayza@espol.edu.ec>
2021-12-14 10:47:48 -0500

    productos

---
+++ migrations/20211214151810-create-producto.js
@@ -0,0 +1,30 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('productos', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      nombre: {
+        type: Sequelize.STRING
+      },
+      cantidad: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('productos');
+  }
+};
---
+++ seeders/20211214153620-productos.js
@@ -0,0 +1,33 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+    for (let i = 0; i <10; i++) {
+      await queryInterface.bulkInsert('Productos', [{
+        nombre: 'Producto '+i,
+        cantidad: 10+i,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }], {});
+    }
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete('Productos', null, {});
+  }
+};
