cc71c925b38b40aea1905429460145b0c705bbf9
sgloayza <sgloayza@espol.edu.ec>
2021-12-13 23:06:51 -0500

    form

--- routes/login.js
+++ routes/login.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
+let bd = {
+  'usuario': 'abc',
+  'contrasenia': '123'
+}
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.render('login', { title: 'Login' });
@@ -8,3 +13,35 @@ router.get('/', function(req, res, next) {
 });
 
 module.exports = router;
+
+router.post('/validate', function(req, res, next) {
+  let usuario = req.body.user;
+  let contrasenia = req.body.password;
+
+  console.log("usuario: ", usuario)
+  console.log("contraseña: ", contrasenia)
+
+  //Validación
+  if(usuario == bd['usuario'] && contrasenia == bd['contrasenia']) {
+    res.redirect('/');
+  } else {
+    res.redirect('/login')
+  }
+
+});
+
+router.get('/validate', function(req, res, next) {
+  let usuario = req.query.user;
+  let contrasenia = req.query.password;
+
+  console.log("usuario: ", usuario)
+  console.log("contraseña: ", contrasenia)
+
+  //Validación
+  if(usuario == bd['usuario'] && contrasenia == bd['contrasenia']) {
+    res.redirect('/');
+  } else {
+    res.redirect('/login')
+  }
+
+});
