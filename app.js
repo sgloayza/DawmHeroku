7ac1bc2bab20a2e0a96891aa16c425a3099b5184
sgloayza <sgloayza@espol.edu.ec>
2021-12-13 20:38:47 -0500

    heroku ultimo

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var serviciosRouter = require('./routes/servicios');
 var contactenosRouter = require('./routes/contactenos');
+var loginRouter = require('./routes/login');
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/servicios', serviciosRouter);
 app.use('/contactenos', contactenosRouter);
+app.use('/login', loginRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
